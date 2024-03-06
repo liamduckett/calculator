47bf8ea669ae57e087dda4c59b359fb3ad74c371
Liam Duckett <liamduckett2@gmail.com>
2024-03-06 10:28:02 +0000

    split the calculator into a tokenizer, parser and resolver

--- app/Calculator.php
+++ app/Calculator.php
@@ -4,6 +4,7 @@ namespace Liamduckett\Calculator;
 
 use Exception;
 use Liamduckett\Calculator\Exceptions\InvalidOperandException;
+use Liamduckett\Calculator\Support\Collection;
 use Liamduckett\Calculator\Support\Str;
 
 class Calculator
@@ -13,13 +14,28 @@ class Calculator
      */
     static function calculate(string $input): int
     {
-        $items = Str::make($input)
+        $tokens = static::tokenize($input);
+
+        $expression = static::parse($tokens);
+
+        return static::resolve($expression);
+    }
+
+    protected static function tokenize(string $input): Collection
+    {
+        return Str::make($input)
             // replace 'x' with '*'
             ->replace(search: 'x', replace: '*')
             // split by operator (accepts + - * and /)
             ->pregSplit('/([+\-*\/])/', flags: PREG_SPLIT_DELIM_CAPTURE);
+    }
 
-        $items = $items->mapWithKeys(function(string $item, int $key) {
+    /**
+     * @throws InvalidOperandException
+     */
+    protected static function parse(Collection $tokens): Collection
+    {
+        return $tokens->mapWithKeys(function(string $item, int $key) {
             return match($key % 2 === 0) {
                 // even keys are operands
                 true => is_numeric($item) ? (int) $item : throw new InvalidOperandException,
@@ -27,26 +43,29 @@ class Calculator
                 false => Operator::from($item),
             };
         });
+    }
 
+    protected static function resolve(Collection $expression): int
+    {
         // until we have a single result
-        while($items->count() > 1) {
+        while($expression->count() > 1) {
             // return the index of the most pressing operation
-            $operatorIndex = $items->searchMultiple([Operator::MULTIPLY, Operator::DIVIDE], default: 1);
+            $operatorIndex = $expression->searchMultiple([Operator::MULTIPLY, Operator::DIVIDE], default: 1);
 
             // extract the needed items for the most pressing operation
-            [$first, $operator, $second] = $items->slice($operatorIndex - 1, 3);
+            [$first, $operator, $second] = $expression->slice($operatorIndex - 1, 3);
 
             // remove the above items and swap them out for the result of the calculation
-            unset($items[$operatorIndex - 1]);
-            $items[$operatorIndex] = $operator->calculate($first, $second);
-            unset($items[$operatorIndex + 1]);
+            unset($expression[$operatorIndex - 1]);
+            $expression[$operatorIndex] = $operator->calculate($first, $second);
+            unset($expression[$operatorIndex + 1]);
 
             // sort by key
-            $items = $items
+            $expression = $expression
                 ->sortByKeys()
                 ->values();
         }
 
-        return $items[0];
+        return $expression[0];
     }
 }
