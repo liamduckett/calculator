57b05af96b1132486d0c24440163b16025e9c08d
Liam Duckett <liamduckett2@gmail.com>
2024-03-06 19:32:52 +0000

    further cleanup the tokenizer with internal methods

--- app/Tokenizer.php
+++ app/Tokenizer.php
@@ -9,45 +9,38 @@ class Tokenizer
     protected int $index = 0;
     protected array $tokens = [];
 
-    public function __construct(
+    static function tokenize(string $input): array
+    {
+        $input = Str::make($input)
+            // allow use of 'x' as '*'
+            ->replace(search: 'x', replace: '*')
+            // allow use of '**' as '^'
+            ->replace(search: '**', replace: '^')
+            // remove whitespace either side
+            ->trim();
+
+        $tokenizer = new self($input);
+
+        return $tokenizer->run();
+    }
+
+    protected function __construct(
         protected Str $input,
     ) {}
 
     function run(): array
     {
         while($this->index < $this->input->length()) {
-            $operand = null;
-
-            if($this->currentCharacter()->is('(')) {
-                $this->incrementCharacter();
-
-                // loop through until we find a closed bracket
-                while($this->currentCharacter()->isnt(')')) {
-                    $operand .= $this->currentCharacter();
-                    $this->incrementCharacter();
-                }
-
-                // skip the closed bracket
-                $this->incrementCharacter();
-                // recursive call for brackets
-                $this->tokens[] = static::tokenize($operand);
-            }
+            $operand = $this->extractBracketedExpressionIfApplicable();
 
             // there may not be an operand here, this could be the end of the string...
             if($this->currentCharacterIsLastCharacter()) {
                 break;
             }
 
-            // only look for regular operand if bracketed one wasn't found
-            if(!$operand) {
-                $operand = '';
-
-                while($this->currentCharacter()->isNumeric()) {
-                    $operand .= $this->currentCharacter();
-                    $this->incrementCharacter();
-                }
-
-                $this->tokens[] = $operand;
+            // only extract an operand if we didn't find a bracketed one already
+            if($operand === null) {
+                $this->extractOperand();
             }
 
             // there may not be an operator, this could be the end of the string...
@@ -57,11 +50,7 @@ class Tokenizer
 
             $this->skipSpaces();
 
-            // operators are only 1 character long
-            $this->tokens[] = $this->currentCharacter()->toString();
-            $this->incrementCharacter();
-
-            $this->skipSpaces();
+            $this->extractOperation();
         }
 
         return $this->tokens;
@@ -90,18 +79,46 @@ class Tokenizer
         $this->index += 1;
     }
 
-    static function tokenize(string $input): array
+    protected function extractBracketedExpressionIfApplicable(): ?string
     {
-        $input = Str::make($input)
-            // allow use of 'x' as '*'
-            ->replace(search: 'x', replace: '*')
-            // allow use of '**' as '^'
-            ->replace(search: '**', replace: '^')
-            // remove whitespace either side
-            ->trim();
+        $operand = null;
 
-        $tokenizer = new self($input);
+        if($this->currentCharacter()->is('(')) {
+            $this->incrementCharacter();
 
-        return $tokenizer->run();
+            // loop through until we find a closed bracket
+            while($this->currentCharacter()->isnt(')')) {
+                $operand .= $this->currentCharacter();
+                $this->incrementCharacter();
+            }
+
+            // skip the closed bracket
+            $this->incrementCharacter();
+            // recursive call for brackets
+            $this->tokens[] = static::tokenize($operand);
+        }
+
+        return $operand;
+    }
+
+    protected function extractOperand(): void
+    {
+        $operand = '';
+
+        while($this->currentCharacter()->isNumeric()) {
+            $operand .= $this->currentCharacter();
+            $this->incrementCharacter();
+        }
+
+        $this->tokens[] = $operand;
+    }
+
+    protected function extractOperation(): void
+    {
+        // operators are only 1 character long
+        $this->tokens[] = $this->currentCharacter()->toString();
+        $this->incrementCharacter();
+
+        $this->skipSpaces();
     }
 }
