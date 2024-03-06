f9c4e2e8de85448d4cd4f4f8e69e524656ea55b3
Liam Duckett <liamduckett2@gmail.com>
2024-03-06 11:21:46 +0000

    swap out regex tokenizer for while loop tokenizer

--- app/Calculator.php
+++ app/Calculator.php
@@ -4,8 +4,9 @@ namespace Liamduckett\Calculator;
 
 use Exception;
 use Liamduckett\Calculator\Exceptions\InvalidOperandException;
+use Liamduckett\Calculator\Exceptions\InvalidOperatorException;
+use Liamduckett\Calculator\Exceptions\MissingLastOperandException;
 use Liamduckett\Calculator\Support\Collection;
-use Liamduckett\Calculator\Support\Str;
 
 class Calculator
 {
@@ -23,11 +24,41 @@ class Calculator
 
     protected static function tokenize(string $input): Collection
     {
-        return Str::make($input)
-            // replace 'x' with '*'
-            ->replace(search: 'x', replace: '*')
-            // split by operator (accepts + - * and /)
-            ->pregSplit('/([+\-*\/])/', flags: PREG_SPLIT_DELIM_CAPTURE);
+        // allow use of 'x' as '*'
+        $input = str_replace('x', '*', $input);
+        // remove spaces
+        $input = str_replace(' ', '', $input);
+
+        $items = [];
+        $item = '';
+        $index = 0;
+
+        // Loop through the string
+        // Look for operand, until a non-numeric character is found
+        // Next character HAS to be an operation
+
+        while($index < strlen($input)) {
+            while(is_numeric($input[$index])) {
+                $item .= $input[$index];
+                $index += 1;
+            }
+
+            // char is no longer numeric
+            // previous chars are first operand
+            $items[] = $item;
+            $item = '';
+
+            // there may not be an operator, this could be the end of the string...
+            if($index === strlen($input)) {
+                break;
+            }
+
+            // operator must be the current character
+            $items[] = $input[$index];
+            $index += 1;
+        }
+
+        return new Collection($items);
     }
 
     /**
@@ -35,7 +66,7 @@ class Calculator
      */
     protected static function parse(Collection $tokens): Collection
     {
-        return $tokens->mapWithKeys(function(string $item, int $key) {
+        $tokens = $tokens->mapWithKeys(function(string $item, int $key) {
             return match($key % 2 === 0) {
                 // even keys are operands
                 true => is_numeric($item) ? (int) $item : throw new InvalidOperandException,
@@ -43,6 +74,12 @@ class Calculator
                 false => Operator::from($item),
             };
         });
+
+        if($tokens[array_key_last($tokens->toArray())] instanceof Operator) {
+            throw new InvalidOperandException;
+        }
+
+        return $tokens;
     }
 
     protected static function resolve(Collection $expression): int
